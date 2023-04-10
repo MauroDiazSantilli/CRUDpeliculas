517e67cb001bfe783a96c3507e98f2ad5f298e87
MauroDS <mauro.diaz.santilli@gmail.com>
2023-04-10 11:10:26 -0300

    Probamos evento click en boton editar

--- js/admin.js
+++ js/admin.js
@@ -1,5 +1,11 @@
 import Pelicula from "./classPelicula.js"
 
+const btnEditar = document.querySelector('#btnEditar')
+btnEditar.addEventListener('click', crearPelicula)
+
 // crear nueva pelicula
 
 let nuevaPelicula = new Pelicula('Super Mario', 'algo', 'url', 'aventura', 2023, '2hs', 'EEUU', '-')
+console.log(nuevaPelicula)
+
+
